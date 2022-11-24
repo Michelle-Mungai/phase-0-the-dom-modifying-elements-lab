7ea58e98888a46265ca8fb522b4f6fe82c01d994
Michelle-Mungai <chellymungai@gmail.com>
2022-11-24 09:52:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.setAttribute("id", 'victory');
+
+const name = "Michelle";
+
+newHeader.innerHTML = `Michelle is the champion`;
